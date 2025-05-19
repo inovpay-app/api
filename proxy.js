27a29f1d231f63d6e02b1c0c257e9cae724eeb16
inovpay-app <inovpay.app@gmail.com>
2025-05-19 16:33:08 -0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const TARGET_API = process.env.TARGET_API || 'https://jsonplaceholder.typicode.com'; // valor default
+const TARGET_API = process.env.TARGET_API || 'https://jsonplaceholder.typicode.com';
 
 const app = express();
 
@@ -9,6 +9,16 @@ app.use('/', createProxyMiddleware({
   target: TARGET_API,
   changeOrigin: true,
   preserveHeaderKeyCase: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // Log para debug (opcional)
+    console.log('[HEADERS RECEBIDOS]');
+    console.log(req.headers);
+
+    // Repassar todos os headers manualmente
+    Object.keys(req.headers).forEach((key) => {
+      proxyReq.setHeader(key, req.headers[key]);
+    });
+  }
 }));
 
 const PORT = process.env.PORT || 3000;
